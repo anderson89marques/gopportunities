0da07b8f356762af04445f4e565d4776089ba09a
anderson89marques <andersonoanjo18@gmail.com>
2024-07-08 18:51:02 -0300

    feat: Add Custom logger and gorm schema.

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/anderson89marques/gopportunities/config"
 	"github.com/anderson89marques/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+
+	if err != nil {
+		fmt.Println(err)
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
